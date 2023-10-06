306712d9919c8f04afb50e6598b2ed4d562e8251
Tomdieu <ivantomdio@gmail.com>
2023-10-06 14:23:56 +0100

    updated Fabrique to take 3 kinds of product

--- Factory Pattern/Exo 2/src/com/factory/FabriqueAbstraite.java
+++ Factory Pattern/Exo 2/src/com/factory/FabriqueAbstraite.java
@@ -1,7 +1,14 @@
 package com.factory;
 
 import com.produit.ProduitA;
+import com.produit.ProduitB;
+import com.produit.ProduitC;
 
 public abstract class FabriqueAbstraite {
     public abstract ProduitA createProduitA();
+
+    public abstract ProduitB createProduitB();
+
+    public abstract ProduitC createProduitC();
+
 }
--- Factory Pattern/Exo 2/src/com/factory/FabriqueConcrete3.java
+++ Factory Pattern/Exo 2/src/com/factory/FabriqueConcrete3.java
@@ -2,9 +2,23 @@ package com.factory;
 
 import com.produit.ProduitA;
 import com.produit.ProduitA3;
+import com.produit.ProduitB;
+import com.produit.ProduitB3;
+import com.produit.ProduitC;
+import com.produit.ProduitC3;
 public class FabriqueConcrete3 extends FabriqueAbstraite{
     @Override
     public ProduitA createProduitA() {
         return new ProduitA3();
+    }
+
+    @Override
+    public ProduitB createProduitB() {
+        return new ProduitB3();
+    }
+
+    @Override
+    public ProduitC createProduitC() {
+        return new ProduitC3();
     }
 }
